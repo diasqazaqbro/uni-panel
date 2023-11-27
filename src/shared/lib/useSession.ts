26e6b59f3c6289f21c35792002f71e2b1350a013
diasqazaqbro <diasqazaqbro@mail.ru>
2023-11-28 02:53:12 +0600

    lol

--- src/shared/lib/useSession.ts
+++ src/shared/lib/useSession.ts
@@ -1,77 +1,71 @@
-// 'use client'
-// import { useState, useEffect } from 'react';
-// import axios from 'axios';
+'use client'
+import { useState, useEffect } from 'react';
 
-// export interface IUserProfile {
-//   avatar: string;
-//   email: string;
-//   favourite_list_id: string;
-//   first_name: string;
-//   last_name: string;
-//   phone_number: string;
-//   recent_list_id: string;
-//   username: string;
-// }
+export interface IUserProfile {
+  avatar: string;
+  email: string;
+  favourite_list_id: string;
+  first_name: string;
+  last_name: string;
+  phone_number: string;
+  recent_list_id: string;
+  username: string;
+}
 
-// interface ISessionHook {
-//   user: IUserProfile | null;
-//   isAuthorized: boolean;
-//   getToken: () => string | null;
-//   setSession: (newToken: string) => void;
-//   clearSession: () => void;
-// }
+interface ISessionHook {
+  user: IUserProfile | null;
+  isAuthorized: boolean;
+  getToken: () => string | null;
+  setSession: (newToken: string) => void;
+  clearSession: () => void;
+}
 
-// const useSession = (): ISessionHook => {
-//   const [user, setUser] = useState<IUserProfile | null>(null);
-//   const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
+const useSession = (): ISessionHook => {
+  const [user, setUser] = useState<IUserProfile | null>(null);
+  const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
 
-//   const getToken = (): string | null => {
-//     if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
-//       return localStorage.getItem('auth_token') || "";
-//     }
-//     return null;
-//   };
+  const getToken = (): string | null => {
+    if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
+      return localStorage.getItem('access_token') || "";
+    }
+    return null;
+  };
 
-//   const setSession = (newToken: string): void => {
-//     localStorage.setItem('auth_token', newToken);
-//   };
+  const setSession = (newToken: string): void => {
+    localStorage.setItem('access_token', newToken);
+  };
 
-//   const clearSession = (): void => {
-//     localStorage.removeItem('auth_token');
-//     setIsAuthorized(false)
-//     setUser(null);
-//   };
+  const clearSession = (): void => {
+    localStorage.removeItem('access_token');
+    setIsAuthorized(false)
+    setUser(null);
+  };
 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const token = getToken();
-//         if (token) {
-//           const response = await axios.get('https://timkaqwerty.pythonanywhere.com/api/uprofile/', {
-//             headers: {
-//               'Authorization': `Token ${token}`,
-//             },
-//           });
-//           if (response.status === 200) {
-//             setIsAuthorized(true);
-//             setUser(response.data.results);
-//           }
-//         }
-//       } catch (error) {
-//         console.error('Ошибка при запросе пользователя:', error);
-//       }
-//     };
+  useEffect(() => {
+    const userToken = getToken()
+    fetch('ваш_серверный_эндпоинт', {
+        method: 'GET', 
+        headers: {
+          'Authorization': `Bearer ${userToken}`,
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.error('Ошибка запроса:', error);
+        });
+  }, []); 
 
-//     fetchData();
-//   }, []); 
+  return {
+    user,
+    getToken,
+    setSession,
+    clearSession,
+    isAuthorized
+  };
+};
 
-//   return {
-//     user,
-//     getToken,
-//     setSession,
-//     clearSession,
-//     isAuthorized
-//   };
-// };
-
-// export default useSession;
+export default useSession;
